82f7d5e3cad4d73fdcdc1d221b694a48565e99ad
Abhishekpandey3417 <abhishekpandey06102002@gmail.com>
2026-05-25 09:20:22 +0530

    test3

--- app.js
+++ app.js
@@ -27,27 +27,24 @@ app.use(express.urlencoded({ extended: true }));
 // CORS
 
 
-app.use(cors({
-  origin: [
-    "https://frontend-transport-managementyi.onrender.com",
-    "http://localhost:5173"
-  ],
-  credentials: true
-}));
+const allowedOrigins = [
+  "https://frontend-transport-managementyi.onrender.com",
+  "http://localhost:5173"
+];
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-            return callback(new Error("Not allowed by CORS: " + origin));
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS: " + origin));
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 
 // SECURITY
 app.use(helmet());
